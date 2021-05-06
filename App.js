892cbb585018d9b9c142f8d5fef4ae39a1f6b668
siddhant235 <siddhant235@gmail.com>
2021-05-06 21:37:57 +0530

    inputbox and button added

--- App.js
+++ App.js
@@ -1,21 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View,TextInput } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app! </Text>
-      <StatusBar style="auto" />
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder ="Your Goal" style={styles.input}/>
+        <Button title="Add"/>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+screen:{
+  padding:50
+},
+inputContainer:{
+  flexDirection:'row',
+  justifyContent:'space-between',
+  alignItems:'center'
+},
+input:{
+  width:'80%',
+  borderColor:'black',
+  borderWidth:1,
+  padding:10
+}
 });
