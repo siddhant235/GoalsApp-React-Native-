6bf6b704381f4ceb21097284c0341a98f3ccc067
siddhant235 <siddhant235@gmail.com>
2021-05-07 22:05:23 +0530

    added components

--- App.js
+++ App.js
@@ -1,14 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { Button, StyleSheet, Text, View,TextInput } from 'react-native';
-
+import React,{useState} from 'react';
+import { Button, StyleSheet, Text, View,TextInput,FlatList} from 'react-native';
+import GoalItem from './components/GoalItem'
+import GoalInput from './components/GoalInput'
 export default function App() {
+ 
+  const [goals,setGoals]=useState([]);
+ 
+  const addGoalHandler=(enteredGoal)=>{
+    setGoals(currentGoals=>[...currentGoals,{
+      id:Math.random().toString(),
+      value:enteredGoal
+    }])
+  }
   return (
     <View style={styles.screen}>
-      <View style={styles.inputContainer}>
-        <TextInput placeholder ="Your Goal" style={styles.input}/>
-        <Button title="Add"/>
-      </View>
+     <GoalInput addGoalHandler={addGoalHandler}/>
+    <FlatList keyExtractor={(item,index)=>item.id} data={goals} renderItem={itemData=><GoalItem title={itemData.item.value}/>}/> 
+    
+     
+      
+    
     </View>
   );
 }
@@ -17,15 +29,6 @@ const styles = StyleSheet.create({
 screen:{
   padding:50
 },
-inputContainer:{
-  flexDirection:'row',
-  justifyContent:'space-between',
-  alignItems:'center'
-},
-input:{
-  width:'80%',
-  borderColor:'black',
-  borderWidth:1,
-  padding:10
-}
+
+
 });
---
+++ components/GoalInput.js
@@ -0,0 +1,30 @@
+import React,{useState} from 'react'
+import {View,TextInput,Button,StyleSheet} from 'react-native'
+const GoalInput = (props) => {
+    const [enteredGoal,setEneterdGoal]=useState('');
+    const goalInputHandler=(enteredText)=>{
+        setEneterdGoal(enteredText)
+      }
+    return (
+        <View style={styles.inputContainer}>
+        <TextInput placeholder ="Your Goal" style={styles.input} onChangeText={goalInputHandler} value={enteredGoal}/>
+        <Button title="Add" onPress={()=>props.addGoalHandler(enteredGoal)}/>
+      </View>
+            
+        
+    )
+}
+const styles=StyleSheet.create({
+    inputContainer:{
+        flexDirection:'row',
+        justifyContent:'space-between',
+        alignItems:'center'
+      },
+      input:{
+        width:'80%',
+        borderColor:'black',
+        borderWidth:1,
+        padding:10
+      }
+})
+export default GoalInput;
